057daa069ea98e771f5ba9835b1ccdca2de7243d
Kenichi Kamiya <kachick1@gmail.com>
2024-11-18 13:28:38 +0900

    fix(nix): Allow multiple nixpkgs in flake inputs

--- lib/modules/manager/nix/extract.spec.ts
+++ lib/modules/manager/nix/extract.spec.ts
@@ -73,4 +73,32 @@ describe('modules/manager/nix/extract', () => {
       ],
     });
   });
+
+  it('returns all nixpkgs', () => {
+    const content = `{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-21.11-darwin";
+  };
+}`;
+
+    const res = extractPackageFile(content);
+
+    expect(res?.deps).toEqual([
+      {
+        depName: 'nixpkgs',
+        currentValue: 'nixos-21.11',
+        datasource: GitRefsDatasource.id,
+        packageName: 'https://github.com/NixOS/nixpkgs',
+        versioning: nixpkgsVersioning,
+      },
+      {
+        depName: 'nixpkgs',
+        currentValue: 'nixpkgs-21.11-darwin',
+        datasource: GitRefsDatasource.id,
+        packageName: 'https://github.com/NixOS/nixpkgs',
+        versioning: nixpkgsVersioning,
+      },
+    ]);
+  });
 });
